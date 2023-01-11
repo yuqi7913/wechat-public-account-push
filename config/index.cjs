1d460dd4347822960c10ecebd4447227e4ecff28
yuqi7913 <49739632+yuqi7913@users.noreply.github.com>
2023-01-11 17:20:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '可爱的恋恋',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oWtUy50a6CfBkN2tL9AVs7aMRD8Q',
+      id: 'oWtUy5wRHhfLI_QNkrq67d3I2qcw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'D37GUdDCHp2oPuFAerneLcLHXhvujlxxCXF9R5El9Is',
+      useTemplateId: '	ba05MblDXX_YVQsiK8y3jZPWTnOXg8VcZZofSzMqIIU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-05',
       festivals: [
